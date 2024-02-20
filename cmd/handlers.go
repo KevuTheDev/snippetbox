96e78b8f7a2bd0197b4b8d920f94019d1ce6ec8c
KevuTheDev <kevin66he@hotmail.com>
2024-02-20 00:54:37 -0500

    Setup Server

---
+++ cmd/handlers.go
@@ -0,0 +1,33 @@
+package main
+
+import {
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/joho/godotenv"
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Snippetbox\n"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Header()
+	w.Write([]byte("Creating a new snippet...\n"))
+}
--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"errors"
-	"html/template"
 	"log"
 	"net/http"
 	"os"
@@ -12,43 +10,6 @@ import (
 	"github.com/joho/godotenv"
 )
 
-type Template struct {
-	tmpl *template.Template
-}
-
-func newTemplate() *Template {
-	return &Template{
-		tmpl: template.Must(template.ParseGlob("views/*.html")),
-	}
-}
-
-type Handler func(w http.ResponseWriter, r *http.Request) error
-
-func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if err := h(w, r); err != nil {
-		// handle returned error here.
-		w.WriteHeader(503)
-		w.Write([]byte("bad"))
-	}
-}
-
-func tesst(w http.ResponseWriter, r *http.Request) error {
-	q := r.URL.Query().Get("err")
-
-	if q != "" {
-		w.Write([]byte("Create a new snippet...\n"))
-		return errors.New(q)
-	}
-
-	log.Println("ERROR POST")
-	w.Write([]byte("Create a new snippet...\n"))
-	return nil
-}
-
-// func (t *Template) Render(w io.Writer, title string, data interface{}, c echo.Context) error {
-// 	return t.tmpl.ExecuteTemplate(w, title, data)
-// }
-
 func main() {
 	// Load Environment Variables
 	err := godotenv.Load()
@@ -57,21 +18,31 @@ func main() {
 	}
 	PORT := os.Getenv("SERVER_PORT")
 
-	chiR := chi.NewRouter()
-	chiR.Use(middleware.Logger)
+	chiRouter := chi.NewRouter()
+	chiRouter.Use(middleware.Logger)
 
-	chiR.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello from Snippetbox\n"))
+	chiRouter.Get("/", home)
+
+	chiRouter.Route("/snippet", func(cr chi.Router) {
+
+		cr.Get("/view", snippetView)
+
+		cr.Post("/create", snippetCreate)
 	})
 
-	chiR.Route("/snippet", func(rr chi.Router) {
-		rr.Get("/view", func(w http.ResponseWriter, r *http.Request) {
-			w.Write([]byte("Display a specific snippet...\n"))
+	chiRouter.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("Route does not exist...\n"))
+	})
+	chiRouter.MethodNotAllowed(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/snippet/create" {
+			w.Header().Set("Allow", http.MethodPost)
+		}
 
-		})
-
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte("Method is not valid\n"))
 	})
 
 	log.Println("Starting server on :" + PORT)
-	http.ListenAndServe(":"+PORT, chiR)
+	http.ListenAndServe(":"+PORT, chiRouter)
 }
