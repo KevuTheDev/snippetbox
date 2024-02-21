6a5f73acd5f1e43adec6eb9f57e9a6bb422a74df
KevuTheDev <kevin66he@hotmail.com>
2024-02-20 19:58:29 -0500

    Working with Templates

--- cmd/handlers.go
+++ cmd/handlers.go
@@ -2,12 +2,30 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from Snippetbox\n"))
+	files := []string{
+		"./ui/html/base.html",
+		"./ui/html/index.html",
+	}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
